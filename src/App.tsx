7c9147920383109823627bf7b6e93ee06aaf5a06
Sadzak23 <sadzak23@gmail.com>
2025-05-19 11:50:59 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import type { FC } from 'react';
+import './styles/App.scss';
+
+const App: FC = () => {
+  return (
+    <div>
+      <h1>Memory game</h1>
+      <p>By Sadzak</p>
+    </div>
+  );
+};
+
+export default App;
