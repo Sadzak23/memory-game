b27cb0373b3dc3e3c393b5de65543179b54ff44a
Sadzak23 <sadzak23@gmail.com>
2025-05-23 20:17:30 +0200

    Create separate Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './styles/App.scss';
 import type { FC } from 'react';
 import { MainProvider } from './contexts/MainContext';
 import { Game } from './pages/Game';
+import { Footer } from './components/Footer';
 
 const App: FC = () => {
   return (
@@ -12,9 +13,7 @@ const App: FC = () => {
           <Game />
         </MainProvider>
       </main>
-      <footer>
-        <p>This game was designed & made by Sadzak © 2025</p>
-      </footer>
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import type { FC } from 'react';
+
+export const Footer: FC = () => (
+  <footer>
+    <p>This game was designed & made by Sadzak © 2025</p>
+  </footer>
+);
