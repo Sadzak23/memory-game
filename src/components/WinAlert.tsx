7444eee604f83f7f3a02be316b805a69a40de8a2
Sadzak23 <sadzak23@gmail.com>
2025-05-26 09:55:37 +0200

    Create favicon

--- src/components/WinAlert.tsx
+++ src/components/WinAlert.tsx
@@ -3,6 +3,6 @@ import type { FC } from 'react';
 export const WinAlert: FC = () => (
   <div className="win-animation">
     <div className="win-text">You Win!</div>
-    <div className="pulse">🏆</div>
+    <div className="trophy">🏆</div>
   </div>
 );
