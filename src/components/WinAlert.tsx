534dba6dac9cdb5092bfcb7ce133dae096c3f241
Sadzak23 <sadzak23@gmail.com>
2025-06-10 16:13:01 +0200

    Make WinAlert fullscreen

--- src/components/WinAlert.tsx
+++ src/components/WinAlert.tsx
@@ -1,8 +1,13 @@
 import type { FC } from 'react';
+import { useMainContext } from 'src/contexts/MainContext';
 
-export const WinAlert: FC = () => (
-  <div className="win-animation">
-    <div className="win-text">You Win!</div>
-    <div className="trophy">🏆</div>
-  </div>
-);
+export const WinAlert: FC = () => {
+  const { onEndGame } = useMainContext();
+
+  return (
+    <div className="win-animation" onClick={onEndGame}>
+      <div className="win-text">You Win!</div>
+      <div className="trophy">🏆</div>
+    </div>
+  );
+};
